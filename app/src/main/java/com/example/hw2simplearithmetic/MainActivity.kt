4f298eb291d6eba0614e1386e6f09e9b520d83a2
arien <aramin@bu.edu>
2024-09-17 19:23:56 -0400

    initial commit

---
+++ app/src/main/java/com/example/hw2simplearithmetic/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.hw2simplearithmetic
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.hw2simplearithmetic.ui.theme.HW2SimpleArithmeticTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+
+}
